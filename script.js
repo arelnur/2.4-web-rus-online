671d94952eca51e132e90aecd8eff11e039563e6
arelnur <arelnur@gmail.com>
2022-10-18 11:43:12 +0400

    M1 Project 20221018 1144

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("Testing");
+var fullName = 'Elnur Arabov'
+var getInitials = function (fullName) {
+  var parts = fullName.split(' ')
+  var initials = ''
+  for (var i = 0; i < parts.length; i++) {
+    if (parts[i].length > 0 && parts[i] !== '') {
+      initials += parts[i][0]
+    }
+  }
+  return initials
+}
+
+console.log(getInitials(fullName))
