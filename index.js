210001f73ea1e4f1e012f0287b205d53f015e85d
arelnur <arelnur@gmail.com>
2023-02-08 12:33:58 +0400

    2023-08-02 тест

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 const fs = require("fs");
 const express = require("express");
 const app = express();
-
 fs.readFile("./employees.json", "utf-8", (err, data) => {
   if (err) {
     console.log(err);
   } else {
     const employees = JSON.parse(data);
-
     app.get("/:id", (req, res) => {
       employees.length >= req.params.id && req.params.id > 0
         ? res
@@ -19,9 +17,7 @@ fs.readFile("./employees.json", "utf-8", (err, data) => {
             )
         : res.status(404).send(`Error: employee "${req.params.id}" not found`);
     });
-
     app.listen(3000, () => {
       console.log("Working");
     });
-  }
-});
+  }});
